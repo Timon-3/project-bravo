290dfe0d45f912d1eefd199f19a7a6d87388b0c9
unknown <timonfrey@bluewin.ch>
2023-01-17 10:49:27 +0100

    init home Site and Admin page

---
+++ tst_room_booking/home/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from home import views
+from home.models import Room
+
+home_list_view = views.HomeListView.as_view(queryset = Room.objects.all()[:5], # :5 limits the results to the five most recent
+context_object_name="room_list",
+template_name="home/home.html",
+)
+
+urlpatterns = [
+    path("", home_list_view, name="home"),
+]
---
+++ tst_room_booking/home/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from home.models import Room
+from django.views.generic import ListView
+# Create your views here.
+
+def home(request):
+    return render(request, "home/home.html")
+
+# Create your views here.
+class HomeListView(ListView):
+    """Renders the home page, with a list of all Rooms"""
+    model = Room
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, 
+        self).get_context_data(**kwargs)
+        return context
