38897b8f582ba1302dd8d942ab7ff12976b66e5f
stevb89 <sb.ghiridone@hotmail.com>
2023-01-17 13:37:45 +0100

    added v1 class Users to models.py

--- tst_room_booking/home/models.py
+++ tst_room_booking/home/models.py
@@ -6,4 +6,13 @@ class Room(models.Model):
     name = models.CharField(max_length=200)
     capacity = models.IntegerField()
     description = models.CharField(max_length=2000)
-    
+
+
+class Users(models.Model):
+    user_ID = models.CharField(max_length=50)
+    psw = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.IntegerField(max_length=20)
+    organization = models.CharField(max_length=50)
