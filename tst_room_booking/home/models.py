92b661ab6603dd87afdfe5c90ef68140cce6ca5b
stevb89 <sb.ghiridone@hotmail.com>
2023-01-17 16:14:43 +0100

    removed (commented) v1-test class Users to models.py

--- tst_room_booking/home/models.py
+++ tst_room_booking/home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import timezone
 
 # Create your models here.
 
@@ -8,11 +9,15 @@ class Room(models.Model):
     description = models.CharField(max_length=2000)
 
 
-class Users(models.Model):
-    user_ID = models.CharField(max_length=50)
-    psw = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=100)
+
+"""
+# can be deleted if we decide to use the Django's built-it authentication system
+    class Users(models.Model):
+    user_ID = models.CharField(verbose_name="User's ID", max_length=50, help_text="User's ID")
+    psw = models.CharField(verbose_name="Password", max_length=100, help_text="Password")
+    email = models.CharField(verbose_name="E-mail", max_length=100, help_text="E-mail")
+    first_name = models.CharField(verbose_name="Password", max_length=100, help_text="Password")
     last_name = models.CharField(max_length=100)
-    phone_number = models.IntegerField(max_length=20)
-    organization = models.CharField(max_length=50)
+    phone_number = models.IntegerField(max_length=20, blank=True)
+    organization = models.CharField(max_length=50, blank=True)
+    created = models.DateTimeField(verbose_name="Creation date", default=timezone.now, help_text="The user was created at that time") """
